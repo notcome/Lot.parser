eab445d1ef77ea766c4be508fc96133cb67e3398
刘闽晟 <notcome@me.com>
2014-11-07 18:47:18 +0800

    rewrite using a state machine idea

--- source.js
+++ source.js
@@ -1,74 +1,104 @@
+/*
+ * three modes
+ * 1. basic mode
+ * 2. type loaded mode
+ * 3. input mode
+ *
+ * basic mode << spacing, type definition, data definition :
+ * spacing: do nothing
+ * type definition: add new type, do nothing
+ * data defintion: set up, into type loaded mode
+ *
+ * type loaoded mode << spacing, delimiter, *
+ * spacing: into basic mode
+ * delimiter: do nothing
+ * *: into input mode, push self into buffer
+ *
+ * input mode << delimiter, *
+ * delimiter: push field, into type loaded mode
+ * *: push self into buffer
+ */
+
 "use strict";
+
 var fs = require('fs');
 var _ = require('underscore');
-var $__0 = require('./util'),
-    extractArray = $__0.extractArray,
-    removeWrappingSpacing = $__0.removeWrappingSpacing;
-var TypeDefRegex = /define ([A-z]+) = (\[[A-z]+(\,[ ]*[A-z]+)*\])/;
+var { extractArray, removeWrappingSpacing } =
+  require('./util');
+
+var TypeDefRegex = /define ([A-z]+) = (\[[A-z]+(\,[ ]*[A-z]+)*\])/
 var DataDefRegex = /([A-z]+) as ([A-z]+|\[[A-z]+(\,[ ]*[A-z]+)*\]|\[\.\.\.\]) =/;
-function LoTParser() {
+
+function LoTParser () {
   this.types = {};
   this.exports = {};
 }
-const Spacing = {type: 'Spacing'};
-const Delimiter = {type: 'Delimiter'};
+
+const Spacing = { type: 'Spacing' };
+const Delimiter = { type: 'Delimiter' };
+
 LoTParser.prototype = {
   types: {},
   exports: {},
   name: null,
   mapping: null,
   buffer: [],
+
   mode: 'basicMode',
-  intoBasicMode: function() {
-    this.mode = 'basicMode';
-  },
-  intoTypeLoadedMode: function() {
-    this.mode = 'typeLoadedMode';
-  },
-  intoInputMode: function() {
-    this.mode = 'inputMode';
-  },
-  parseLine: function(line) {
-    this[this.mode](line);
-  }
-};
-LoTParser.prototype.basicMode = function(line) {
+  intoBasicMode: function () { this.mode = 'basicMode'; },
+  intoTypeLoadedMode: function () { this.mode = 'typeLoadedMode'; },
+  intoInputMode: function () { this.mode = 'inputMode'; },
+
+  parseLine: function (line) { this[this.mode](line); }
+}
+
+LoTParser.prototype.basicMode = function (line) {
   if (line == Spacing)
     return;
+
   let matchTypeDef = TypeDefRegex.exec(line);
   if (matchTypeDef != null)
     return this.parseTypeDef(matchTypeDef);
+
   let matchDatadef = DataDefRegex.exec(line);
   if (matchDatadef != null) {
     this.parseDataDef(matchDatadef);
     return this.intoTypeLoadedMode();
   }
+
   throw 'unexpected line in basic mode';
-};
-LoTParser.prototype.typeLoadedMode = function(line) {
+}
+
+LoTParser.prototype.typeLoadedMode = function (line) {
   if (line == Spacing)
     return this.intoBasicMode();
+
   if (line == Delimiter)
     return;
+
   this.pushField(line);
   return this.intoInputMode();
-};
-LoTParser.prototype.inputMode = function(line) {
+}
+
+LoTParser.prototype.inputMode = function (line) {
   if (line == Delimiter) {
     this.parseBuffer();
     return this.intoTypeLoadedMode();
   }
+
   this.pushField(line);
-};
-LoTParser.prototype.parse = function(data) {
-  let preprocessed = data.split('\n').map(removeWrappingSpacing).map((function(line) {
-    return line.length ? line : Spacing;
-  })).map((function(line) {
-    return line != '---' ? line : Delimiter;
-  }));
+}
+
+LoTParser.prototype.parse = function (data) {
+  let preprocessed = data
+  .split('\n')
+  .map(removeWrappingSpacing)
+  .map(line => line.length ? line : Spacing)
+  .map(line => line != '---' ? line : Delimiter)
+
   try {
     var line;
-    for (line = 0; line < preprocessed.length; line++)
+    for (line = 0; line < preprocessed.length; line ++)
       this.parseLine(preprocessed[line]);
   } catch (err) {
     throw {
@@ -76,13 +106,15 @@ LoTParser.prototype.parse = function(data) {
       error: err
     };
   }
-};
-LoTParser.prototype.parseTypeDef = function(match) {
+}
+
+LoTParser.prototype.parseTypeDef = function (match) {
   let name = match[1];
   let mapping = extractArray(match[2]);
   this.types[name] = mapping;
-};
-LoTParser.prototype.parseDataDef = function(match) {
+}
+
+LoTParser.prototype.parseDataDef = function (match) {
   if (match[2] == '[...]')
     this.mapping = 'array';
   else if (match[2][0] == '[')
@@ -93,29 +125,33 @@ LoTParser.prototype.parseDataDef = function(match) {
       throw 'Type doesn\'t exist.';
     this.mapping = $mapping;
   }
+
   this.name = match[1];
   this.exports[match[1]] = [];
-};
-LoTParser.prototype.pushField = function(line) {
+}
+
+LoTParser.prototype.pushField = function (line) {
   this.buffer.push(line);
-};
-LoTParser.prototype.parseBuffer = function() {
-  let $__1 = this,
-      name = $__1.name,
-      exports = $__1.exports,
-      mapping = $__1.mapping,
-      buffer = $__1.buffer;
+}
+
+LoTParser.prototype.parseBuffer = function () {
+  let {name, exports, mapping, buffer} = this;
+
   if (mapping instanceof Array) {
     if (mapping.length != buffer.length)
       throw 'Data definition hasn\'t finished.';
+
     exports[name].push(_.object(mapping, buffer));
-  } else
+  }
+  else
     exports[name].concat(buffer);
+
   this.buffer = [];
   return this.intoTypeLoadedMode();
-};
-module.exports = function(data) {
+}
+
+module.exports = function (data) {
   var parser = new LoTParser();
   parser.parse(data);
   return parser.exports;
-};
+}
---
+++ util.js
@@ -0,0 +1,35 @@
+function extractArray (str) {
+  function work (str) {
+    var i = 0;
+    var datum;
+    while (str[i] >= 'A' && str[i] <= 'z')
+      i ++;
+    datum = str.slice(0, i);
+
+    if (i == str.length)
+      return datum;
+
+    i ++;
+    while (str[i] == ' ' || str[i] == '\t')
+      i ++;
+    return [datum].concat(work(str.slice(i)));
+  }
+
+  return work(str.slice(1, -1));
+}
+
+function removeWrappingSpacing (line) {
+  var preSp = 0;
+  while (line[preSp] == ' ' || line[preSp] == '\t')
+    preSp ++;
+  
+  var sufSp = line.length - 1;
+  while (line[sufSp] == ' ' || line[sufSp] == '\t')
+    sufSp --;
+  sufSp ++;
+
+  return line.slice(preSp, sufSp);
+}
+
+exports.extractArray = extractArray;
+exports.removeWrappingSpacing = removeWrappingSpacing;
